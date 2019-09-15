07de9f3581d4624eadc41c5eacc9f4b54463252a
Jorge Rivera <jrdeveloper71@gmail.com>
2019-09-15 19:57:42 -0400

    add screenshots-slider control arrows and styles

--- src/components/project-item/screenshots-slider/screenshots-slider.component.jsx
+++ src/components/project-item/screenshots-slider/screenshots-slider.component.jsx
@@ -1,26 +1,34 @@
 import React, { useState } from 'react';
 import './screenshots-slider.styles.scss';
+// Modules
+import usePrevious from '../../../utils/usePrevious';
 import { useTransition, animated } from 'react-spring';
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
 
 const ScreenshotsSlider = ({ screenshots: { desktop, mobile } }) => {
   const [index, setIndex] = useState(desktop.length * 10000);
+  const prevIndex = usePrevious(index);
+  const isNextSlide = prevIndex < index;
 
   const transition = useTransition(desktop[index % desktop.length], null, {
     initial: {
       opacity: 1,
       transform: 'translateX(0px)'
     },
-    from: rightPosHidden,
+    from: isNextSlide ? rightPosHidden : leftPosHidden,
     enter: centerPosShown,
-    leave: leftPosHidden,
+    leave: isNextSlide ? leftPosHidden : rightPosHidden,
     config: {
       mass: 1.3,
       clamp: true
     }
   });
 
+  const nextSlide = () => setIndex(index + 1);
+  const previousSlide = () => setIndex(index - 1);
+
   return (
-    <div className="screenshots-slider" onClick={() => setIndex(index + 1)}>
+    <div className="screenshots-slider">
       {transition.map(({ item, key, props }) => (
         <animated.img
           className="screenshot-item"
@@ -30,6 +38,15 @@ const ScreenshotsSlider = ({ screenshots: { desktop, mobile } }) => {
           alt="screenshot"
         />
       ))}
+      <div className="arrows">
+        <div className="left-arrow" onClick={previousSlide}>
+          <Icon icon="chevron-left" />
+        </div>
+        <div className="zoom-in" />
+        <div className="right-arrow" onClick={nextSlide}>
+          <Icon icon="chevron-right" />
+        </div>
+      </div>
     </div>
   );
 };
